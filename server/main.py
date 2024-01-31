7cf9e794f77a06db7493e9d1a628d8ceb166610f
Tejas Jagtap <111847506+ttejas16@users.noreply.github.com>
2024-01-31 14:48:12 +0530

    test commit

--- server/main.py
+++ server/main.py
@@ -19,7 +19,7 @@ def get_resume_data(prompt):
     response = model.generate_content(prompt);
     
     print(response.text)
-    return 
+    return ["some thing changed"]
 
 if __name__ == "__main__":
     app.run(debug=True)
